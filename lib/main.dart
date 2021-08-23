90fd95a08d45d0a0ea52784c99ed7b0537bc5e25
Luwa <luwanhika@hotmail.com>
2021-08-22 22:04:14 -0300

    create layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 import 'ui/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        ),
+      ),
+    ),
+  );
 }
-
-
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,14 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key key }) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   int _offset = 0;
@@ -19,10 +18,12 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null)
-    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=1O0A6IHoLIFCgmD1tqPQ9qZph0uP4dVV&limit=20&rating=g");
-    else 
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=1O0A6IHoLIFCgmD1tqPQ9qZph0uP4dVV&q=$_search&limit=20&offset=$_offset&rating=g&lang=en");
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=1O0A6IHoLIFCgmD1tqPQ9qZph0uP4dVV&limit=20&rating=g");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=1O0A6IHoLIFCgmD1tqPQ9qZph0uP4dVV&q=$_search&limit=20&offset=$_offset&rating=g&lang=en");
 
     return json.decode(response.body);
   }
@@ -41,9 +42,53 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.gif"),
         centerTitle: true,
       ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(14),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise Aqui!",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200,
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5,
+                      ),
+                    );
+                    default:
+                      if(snapshot.hasError) return Container();
+                      else return _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+
   }
 }
